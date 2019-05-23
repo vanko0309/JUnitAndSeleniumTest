4242addff8477c861ecf7824992dc547138c6172
Ivan Daskalov <ivan.daskalov@aptsys.net>
2019-05-24 01:05:43 +0300

    exclude junit from third-project

--- src/test/java/selenium/UiTest.java
+++ src/test/java/selenium/UiTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class UiTest {
 	 WebDriver driver;
